378be2451aa2cad51571d0dfdc0e702f3da30baf
Yullia <fizmatka908990@gmail.com>
2022-05-07 14:58:17 +0300

    added hw-13

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import { users } from "./data";
+
+const container = document.querySelector(".container");
+
+const createHtmlBlock = ({
+  id,
+  name,
+  age,
+  email,
+  country,
+  company,
+  birthday,
+}) =>
+  `
+           <span> № ${id} </span>
+           <span> <b> name: </b>${name} </span>
+           <span> <b> age: </b>${age} </span>
+           <span> <b> email: </b>${email} </span>
+           <span> <b> country: </b>${country} </span>
+           <span> <b> company: </b>${company} </span>
+           <span> <b> birthday: </b> <time>${birthday}</time> </span>
+`;
+
+const render = () => {
+  const rows = users.map((user) => {
+    const row = document.createElement("div");
+    row.className = "userRow";
+    row.innerHTML = createHtmlBlock(user);
+
+    return row;
+  });
+
+  container.append(...rows);
+};
+
+render();
